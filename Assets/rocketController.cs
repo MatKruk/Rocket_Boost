1bbc466fe3ffc37b6650017c8ff6c4e755267133
MatK <contactmatk@gmail.com>
2018-02-08 17:12:35 +0000

    Base for the game loop setup

--- Assets/rocketController.cs
+++ Assets/rocketController.cs
@@ -8,8 +8,12 @@ public class rocketController : MonoBehaviour {
     Rigidbody rigidBody;
     AudioSource boosterSource;
 
-	// Use this for initialization
-	void Start () {
+    [Range(0.0f, 256.0f)]
+    [SerializeField] float rcsThrust = 100.0f;
+    [Range(0.0f, 256.0f)]
+    [SerializeField] float mainThrust = 100.0f;
+    // Use this for initialization
+    void Start () {
 
         rigidBody = GetComponent<Rigidbody>();
         boosterSource = GetComponent<AudioSource>();
@@ -21,29 +25,67 @@ public class rocketController : MonoBehaviour {
         ProcessInput();
 	}
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Friendly collision");
+                break;
+            case "Fuel":
+                print("Fuel collision");
+                //todo Increase fuel level
+                break;
+            case "Finish":
+                print("Finish");
+                //todo finish game loop
+                break;
+            default:
+                print("Death collision");
+                //todo Kill player
+                break;
+        }
+    }
+
     private void ProcessInput()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
 
-            if (!boosterSource.isPlaying)
+            if (!boosterSource.isPlaying) // stop audio layering
             {
                 boosterSource.Play();
-            } 
+            }
         }
         else
         {
             boosterSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual rotation control.
+
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) // Accept rotational input alongside main thruster input.
         {
-            transform.Rotate(Vector3.forward);
+            
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            
-            transform.Rotate(-Vector3.forward);
+
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         }
+        rigidBody.freezeRotation = false; // resume physics rotation control.
     }
 }
